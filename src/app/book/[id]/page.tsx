d8b4cda0d61abf10eeee8127668de62807924e30
Joowon <kimjw0913@gmail.com>
2025-01-06 15:38:53 +0900

    404 not found 페이지 추가 및 정적경로 설정

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -1,9 +1,20 @@
+import { notFound } from "next/navigation";
 import style from "./page.module.css";
+
+// generateStaticParams로 설정한 파라미터 외에 다른 id로 접속할 시 404 not found로 설정
+// export const dynamicParams = false;
+
+export function generateStaticParams () {
+  return [{ id: "1" }, { id: "2" },{ id: "3" }]
+}
 
 export default async function Page({ params }: { params: Promise<{ id: string | string[] }> }) {
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${(await params).id}`);
 
   if(!response.ok) {
+    if(response.status === 404) {
+      notFound();
+    }
     return <div>오류가 발생했습니다...</div>;
   }
 
